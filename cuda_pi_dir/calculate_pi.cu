a335cdc0783a6d086d33cbbed3ced3dd66ae72ef
Mosè Giordano <m.giordano@ucl.ac.uk>
2026-02-12 17:21:16 +0000

    Refactor the CUDA C++ code
    This uses clearer variables names, and a large number of blocks

--- cuda_pi_dir/calculate_pi.cu
+++ cuda_pi_dir/calculate_pi.cu
@@ -23,9 +23,11 @@ __global__ void calcpi(double step, long work_per_thread, double *results) {
 }
 
 int main( int argc, char **argv ) {
-   long num_steps = 1000000000;
    double result;
-   int threads = 1000; // threads needs to dived num_steps!
+   int blocks = 4096;
+   int threads_per_block = 512;
+   int tot_threads = blocks * threads_per_block;
+   long num_steps = 1099511627776; // must be a multiple of tot_threads
 
    cout.precision(numeric_limits<double>::digits10+2);
 
@@ -33,7 +35,7 @@ int main( int argc, char **argv ) {
       num_steps = atol(argv[1]);
    }
    if (argc > 2) {
-      threads = atol(argv[2]);
+      threads_per_block = atol(argv[2]);
    }
 
    double step, pi;
@@ -41,19 +43,17 @@ int main( int argc, char **argv ) {
 
    cout << "Calculating PI using:" << endl <<
            "  " << num_steps << " slices" << endl <<
-           "  " << threads << " CUDA threads" << endl;
+           "  " << threads_per_block << " CUDA threads" << endl;
 
    double *sum, *d_sum;
-   size_t size = threads*sizeof(double);
+   size_t size = tot_threads*sizeof(double);
    step = 1.0 / num_steps;
-   long work_per_thread = num_steps / threads;
+   long work_per_thread = num_steps / tot_threads;
    sum = (double*)malloc(size);
 
-   cudaMalloc((void**)&d_sum, size);
-   int threadsPerBlock = 128;
-   int numBlocks = (threads + threadsPerBlock - 1) / threadsPerBlock;
    timer.start();
-   calcpi<<<numBlocks, threadsPerBlock>>>(step, work_per_thread, d_sum);
+   cudaMalloc((void**)&d_sum, size);
+   calcpi<<<blocks, threads_per_block>>>(step, work_per_thread, d_sum);
    cudaDeviceSynchronize();
    timer.stop();
    cudaMemcpy(sum, d_sum, size, cudaMemcpyDeviceToHost);
@@ -61,7 +61,7 @@ int main( int argc, char **argv ) {
 
    result = 0.0;
 
-   for (int i=0; i<threads; i++) {
+   for (int i=0; i<tot_threads; i++) {
       result +=sum[i];
    }
    pi = result * step;
